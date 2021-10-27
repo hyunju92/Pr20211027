e2d5e9d67c93b507ff310b5942ee11e0112634f6
leehyunju <hyunju_e521@hanmail.net>
2021-10-28 00:55:23 +0900

    [프로젝트 구조] main 액티비티 내부 - navigation, drawer 설정중

---
+++ app/src/main/java/hyunju/com/pr20211027/MainFragment.kt
@@ -0,0 +1,23 @@
+package hyunju.com.pr20211027
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import hyunju.com.pr20211027.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater, R.layout.fragment_main, container, false).apply {}
+        return binding.root
+    }
+}
