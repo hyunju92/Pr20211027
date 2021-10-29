6f349a9e9d15eb27aa922551fcf7e81e9aa10422
leehyunju <hyunju_e521@hanmail.net>
2021-10-29 23:06:17 +0900

    [network] ResData type 변경

--- app/src/main/java/hyunju/com/pr20211027/main/network/ResMainData.kt
+++ app/src/main/java/hyunju/com/pr20211027/main/network/ResMainData.kt
@@ -1,18 +1,18 @@
 package hyunju.com.pr20211027.main.network
 
 data class ResMainData(
-    val result: String,
-    val data: List<Item>
+    val result: String?,
+    val data: List<Item>?
 )
 
 data class Item(
-    val viewtype: String,
-    val data: ProductItem
+    val viewtype: String?,
+    val data: ProductItem?
 )
 
 data class ProductItem(
-    val image: String,
-    val price: String,
-    val name: String,
-    val detail: String
+    val image: String?,
+    val price: String?,
+    val name: String?,
+    val detail: String?
 )
