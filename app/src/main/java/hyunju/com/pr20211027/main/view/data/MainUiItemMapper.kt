ce30431507da80a719ecd1b30383a8a8252d9795
leehyunju <hyunju_e521@hanmail.net>
2021-10-30 04:35:58 +0900

    [vm, view] api response 값 ui data 로 파싱해주는 부분 수정

--- app/src/main/java/hyunju/com/pr20211027/main/view/data/MainUiItemMapper.kt
+++ app/src/main/java/hyunju/com/pr20211027/main/view/data/MainUiItemMapper.kt
@@ -1,21 +1,50 @@
 package hyunju.com.pr20211027.main.view.data
 
+import hyunju.com.pr20211027.main.network.ProductItem
 import hyunju.com.pr20211027.main.network.ResMainData
 
-fun ResMainData.toMainUiItemList() : List<MainUiItem> {
+fun ResMainData.toMainUiItemList(): List<MainUiItem> {
     val list = mutableListOf<MainUiItem>()
+    var countProductItem = 0
 
     data?.forEach {
         val type = safeUnboxing(it.viewtype)
-        val prodItem = it.data?:return@forEach
+        val prodItem = it.data ?: return@forEach
 
-        when(type)  {
+        when (type) {
             "image" -> list.add(MainImageItem(prodItem))
-            "productItem" -> list.add(MainProdSingleItem(prodItem))
+            "productItem" -> {
+                getItemFromProductItem(prodItem, ++countProductItem)?.let{ mainUiItem ->
+                    list.add(mainUiItem)
+                }
+            }
+
         }
     }
 
     return list
+}
+
+// viewtype이 productItem일 경우,
+// 3번째까지는 싱글로 (1줄에1개)
+// 4개부터는 더블로 표시하도록 함 (1줄에2개)
+var prod1: ProductItem? = null
+private fun getItemFromProductItem(prodItem: ProductItem, countProductItem: Int): MainUiItem? {
+    return if (countProductItem < 4) {
+        MainProdSingleItem(prodItem)
+
+    } else {
+        if (prod1 == null) {
+            prod1 = prodItem
+            null
+        } else {
+            val temp = prod1
+            prod1 = null
+            MainProdDoubleItem(temp!!, prodItem)
+        }
+
+    }
+
 }
 
 
